5d7d3ebeff410909e5d89f486071c1a3645a9566
Pando13 <69055445+Pando13@users.noreply.github.com>
2022-05-18 23:24:28 +0200

    Update TestOrario.java

--- TestOrario.java
+++ TestOrario.java
@@ -107,6 +107,52 @@ class TestOrario{
 				break;
 			
 			case 7:
+				System.out.println("Inserisci la Nazione: ");
+				nome_temp=input.nextLine();
+				temp = 0;
+
+				for(cont = 0; cont < i; cont++){
+					if(orari[cont].nazione.equals(nome_temp)){
+						temp++;
+					}
+				}
+				String subOrari[]=new String[temp];
+				temp=0;
+				for(cont = 0; cont < i; cont++){
+					if(orari[cont].nazione.equals(nome_temp)){
+						subOrari[temp] = orari[cont].getTime();
+						temp++;
+					}
+				}
+				
+				if(subOrari.length == 0){
+					System.out.println("Non ci sono fusi orari");
+					break;
+				}
+
+				if(subOrari.length == 1){
+					System.out.println("è presente 1 fuso orario");
+					break;
+				}
+
+				Orario temp2[] = new Orario[subOrari.length];
+				int j = 0;
+				for (int k=0; k<subOrari.length-1; k++){
+					if (subOrari[k].fullTime != subOrari[k+1].fullTime){
+						temp2[j++] = subOrari[k];
+					}
+				}
+				temp2[j++] = subOrari[subOrari.length-1];
+				// Changing original array
+				for (int k=0; k<j; k++){
+					subOrari[k] = temp2[k];
+				}
+
+				if(j == 0){
+					System.out.println("Non sono presenti fuso orari diversi, solo uno");
+				}else{
+					System.out.println("Sono presenti: " + j + "fusi orari diversi");
+				}
 				break;
 
 			default:
