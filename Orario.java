9b1e27bfc78999a311cfecf7c90f3c16fa7d00fc
fpetranzan <francesco.petranzan@gmail.com>
2022-05-19 22:06:44 +0200

    refactoring codice

--- Orario.java
+++ Orario.java
@@ -1,18 +1,21 @@
-class Orario {
-    String nome, nazione;
-    int ore, minuti, secondi;
-    Boolean is_solare;
+public class Orario {
+    private String citta;
+    private String nazione;
+    private int ore;
+    private int minuti;
+    private int secondi;
+    private Boolean is_solare;
 
-    public Orario(String nome, int ore, int minuti, int secondi, String nazione){
-        this.nome=nome;
-        this.is_solare=true;
-        this.ore=ore;
-        this.minuti=minuti;
-        this.secondi=secondi;
-        this.nazione=nazione;
+    public Orario(String citta, String nazione, int ore, int minuti, int secondi, Boolean is_solare) {
+        this.citta = citta;
+        this.nazione = nazione;
+        this.ore = ore;
+        this.minuti = minuti;
+        this.secondi = secondi;
+        this.is_solare = is_solare;
     }
 
-    void solare(){
+    public void solare(){
         if (!this.is_solare) {
             if (this.ore==0) {
                 this.ore=23;
@@ -26,7 +29,7 @@ class Orario {
         }
     }
 
-    void legale(){
+    public void legale(){
         if (this.is_solare) {
             if (this.ore==23) {
                 this.ore=0;
@@ -40,11 +43,59 @@ class Orario {
         }
     }
 
-    void visualizza(int temp){
-        System.out.println("orario "+temp+": "+this.ore+":"+this.minuti+":"+this.secondi+", citta' = "+this.nome+", nazione = "+this.nazione+", e' solare = "+this.is_solare);
+    public void visualizzaOrario(int numOrario){
+        System.out.println("orario "+numOrario+": "+this.ore+":"+this.minuti+":"+this.secondi+", citta' = "+this.citta+", nazione = "+this.nazione+", e' solare = "+this.is_solare);
     }
-    
-    String getTime(){
+
+    public String getTime(){
         return this.ore+":"+this.minuti+":"+this.secondi;
+    }
+
+    public String getCitta() {
+        return citta;
+    }
+
+    public void setCitta(String citta) {
+        this.citta = citta;
+    }
+
+    public String getNazione() {
+        return nazione;
+    }
+
+    public void setNazione(String nazione) {
+        this.nazione = nazione;
+    }
+
+    public int getOre() {
+        return ore;
+    }
+
+    public void setOre(int ore) {
+        this.ore = ore;
+    }
+
+    public int getMinuti() {
+        return minuti;
+    }
+
+    public void setMinuti(int minuti) {
+        this.minuti = minuti;
+    }
+
+    public int getSecondi() {
+        return secondi;
+    }
+
+    public void setSecondi(int secondi) {
+        this.secondi = secondi;
+    }
+
+    public Boolean getIs_solare() {
+        return is_solare;
+    }
+
+    public void setIs_solare(Boolean is_solare) {
+        this.is_solare = is_solare;
     }
 }
